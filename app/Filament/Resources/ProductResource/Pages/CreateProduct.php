a9d2e98a171c0c316e4e272a6b00a6b3f81227dc
PovilasKorop <povilask007@gmail.com>
2022-06-13 15:27:45 +0300

    Lesson 3 - redirect to index

--- app/Filament/Resources/ProductResource/Pages/CreateProduct.php
+++ app/Filament/Resources/ProductResource/Pages/CreateProduct.php
@@ -8,4 +8,9 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateProduct extends CreateRecord
 {
     protected static string $resource = ProductResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
--- app/Filament/Resources/ProductResource/Pages/EditProduct.php
+++ app/Filament/Resources/ProductResource/Pages/EditProduct.php
@@ -8,4 +8,9 @@ use Filament\Resources\Pages\EditRecord;
 class EditProduct extends EditRecord
 {
     protected static string $resource = ProductResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
