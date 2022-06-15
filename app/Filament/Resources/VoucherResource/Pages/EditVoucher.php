8818b99a2e5912514e6455644b567828fa7f1e38
PovilasKorop <povilask007@gmail.com>
2022-06-15 10:35:12 +0300

    Lesson 15 - prevent edit form on condition

--- app/Filament/Resources/VoucherResource/Pages/EditVoucher.php
+++ app/Filament/Resources/VoucherResource/Pages/EditVoucher.php
@@ -8,4 +8,13 @@ use Filament\Resources\Pages\EditRecord;
 class EditVoucher extends EditRecord
 {
     protected static string $resource = VoucherResource::class;
+
+    protected function beforeFill()
+    {
+        if ($this->record->payments()->exists()) {
+            $this->notify('danger', 'You cannot edit the voucher after it has been used');
+
+            $this->redirect($this->getResource()::getUrl('index'));
+        }
+    }
 }
