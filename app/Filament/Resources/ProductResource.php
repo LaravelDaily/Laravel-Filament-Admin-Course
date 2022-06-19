fe31f3e263a77cca7ea83e38c9a4fc56e8acfbde
PovilasKorop <povilask007@gmail.com>
2022-06-19 08:12:41 +0300

    Lesson 24d - menu navigation badge

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -69,4 +69,9 @@ class ProductResource extends Resource
             'edit' => Pages\EditProduct::route('/{record}/edit'),
         ];
     }
+
+    protected static function getNavigationBadge(): ?string
+    {
+        return self::getModel()::count();
+    }
 }
