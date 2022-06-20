ab98ddf3e5c7790f5f4696f857cbef314ca4e140
PovilasKorop <povilask007@gmail.com>
2022-06-20 10:39:49 +0300

    Lesson 29 - Roles permissions policies

---
+++ app/Policies/ProductPolicy.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Product;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class ProductPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function create(User $user)
+    {
+        return $user->role_id == 2;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function update(User $user, Product $product)
+    {
+        return $user->role_id == 2;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function delete(User $user, Product $product)
+    {
+        return $user->role_id == 2;
+    }
+
+    public function deleteAny(User $user)
+    {
+        return $user->role_id == 2;
+    }
+}
